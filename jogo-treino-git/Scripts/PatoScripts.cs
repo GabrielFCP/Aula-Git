5d9f12af49dfe9a4944a9e6530147ff1d55f673d
Felipoppops <felipebehalves@gmail.com>
2026-06-29 14:09:04 -0300

    Pato consegue se mover, mas não anda (Input)

---
+++ jogo-treino-git/Scripts/PatoScripts.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class PatoScripts : Node2D
+{
+	Vector2 input;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		GetInput();
+	}
+
+	private void GetInput()
+	{
+		Vector2 Tempinput = Input.GetVector("Esquerda", "Direita", "Cima", "Baixo");
+		Vector2 input = new Vector2 (Tempinput.X * 2, 0).Normalized();
+	}
+
+}
