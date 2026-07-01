5d8063cfbda72a1af2d7c7bd23744fed968f607a
Celly <bacellarcelly@gmail.com>
2026-07-01 19:58:54 -0300

    alteracoes no script PatoScript

--- jogo-treino-git/Scripts/PatoScripts.cs
+++ jogo-treino-git/Scripts/PatoScripts.cs
@@ -3,6 +3,18 @@ using System;
 
 public partial class PatoScripts : Node2D
 {
+
+	/// <summary>
+	/// fazendo umas variaveis aleatórias
+	/// </summary>
+	/// 
+	[Export]
+	RigidBody2D PatoRb;
+	private float vida = 100.0f;
+	[Export]
+	public float dano = 50.0f;
+	public bool PatoIsAlive = true;
+
 	Vector2 input;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -19,6 +31,12 @@ public partial class PatoScripts : Node2D
 	{
 		Vector2 Tempinput = Input.GetVector("Esquerda", "Direita", "Cima", "Baixo");
 		Vector2 input = new Vector2 (Tempinput.X * 2, 0).Normalized();
+	}
+
+	private void PrintFunction()
+	{
+		GD.Print("Teste conflito");
+		
 	}
 
 }
